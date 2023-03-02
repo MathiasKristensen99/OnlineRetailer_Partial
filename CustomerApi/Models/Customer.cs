901700c1db04867525fae626d1067d30bfb0898b
Mathias Kristensen <mathiaskristensen52@gmail.com>
2023-03-02 22:22:44 +0100

    Added customer api

---
+++ CustomerApi/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerApi.Models
+{
+    public class Customer
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string BillingAddress { get; set; }
+        public string ShippingAddress { get; set; }
+        public double CreditStanding { get; set; }
+    }
+}
