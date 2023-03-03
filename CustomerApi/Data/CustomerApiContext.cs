b37999b2b02f04440245d7877a38c9f80e6c29e4
julianswagger <72072509+julianswagger@users.noreply.github.com>
2023-03-03 11:04:47 +0100

    Added ControllerApiContext

--- CustomerApi/Data/CustomerApiContext.cs
+++ CustomerApi/Data/CustomerApiContext.cs
@@ -1,6 +1,12 @@
+using CustomerApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace CustomerApi.Data;
 
 public class CustomerApiContext : DbContext
 {
-    
+    public CustomerApiContext(DbContextOptions<CustomerApiContext> options)
+        : base(options) { }
+
+    public DbSet<Customer> Customer { get; set; }
 }
